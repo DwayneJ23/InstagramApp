70ed8d7a1992ba32f1d7fb39eb19b8072cb52fd8
Dwayne Johnson <dwaynejohnson@mac.home>
2017-03-18 02:07:06 -0400

    First commit

---
+++ InstagramApp/CustomPostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomPostTableViewCell.swift
+//  InstagramApp
+//
+//  Created by Dwayne Johnson on 3/17/17.
+//  Copyright © 2017 GroupDiscountApp. All rights reserved.
+//
+
+import UIKit
+
+class CustomPostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ InstagramApp/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  InstagramApp
+//
+//  Created by Dwayne Johnson on 3/17/17.
+//  Copyright © 2017 GroupDiscountApp. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+
+}
